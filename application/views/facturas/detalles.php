99e66ab2351e36780755c5574f12087acbaf4e7d
Francisco Rangel <frankjrangel@gmail.com>
2015-08-16 22:13:50 -0430

    Cambios estructurales

---
+++ application/views/facturas/detalles.php
@@ -0,0 +1,48 @@
+<a href="<?php echo site_url('facturas') ?>"><button class="btn btn-default">Regresar</button></a>
+
+<h1>Detalles de Factura #<?php echo $numero_factura ?></h1> <!--Agregar establecimiento-->
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('facturas/detalles', 'class="form-inline"') ?>
+  <label for="numero_factura" class="sr-only">Nro Factura:</label>
+  <input class="sr-only" type="input" name="numero_factura" value="<?php echo set_value('numero_factura', $numero_factura) ?>" readonly>
+
+  <label for="marca" class="sr-only">Marca</label>
+  <input class="form-control" type="input" name="marca" value="<?php echo set_value('marca') ?>" placeholder="Marca">
+
+  <label for="Nombre" class="sr-only">Nombre</label>
+  <input class="form-control" type="input" name="nombre" value="<?php echo set_value('nombre') ?>" placeholder="Nombre">
+
+  <label for="precio" class="sr-only">Precio</label>
+  <input class="form-control" type="input" name="precio" value="<?php echo set_value('precio') ?>" placeholder="Precio">
+
+  <label for="cantidad" class="sr-only">Cantidad</label>
+  <input class="form-control" type="input" name="cantidad" value="<?php echo set_value('cantidad') ?>" placeholder="Cantidad">
+
+  <input class="btn btn-default btn-primary" type="submit" name="submit" value="Agregar" />
+
+</form>
+
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>Nombre</th>
+      <th>Precio</th>
+      <th>Cantidad</th>
+    </thead>
+    <tbody>
+  <?php foreach ($productos as $producto): ?>
+    <tr>
+      <td><?php echo $producto['nombre'] ?></td>
+      <td><?php echo $producto['precio'] ?></td>
+      <td><?php echo $producto['cantidad'] ?></td>
+      <td>
+        <a href="<?php //Ir a pantalla de edicion de producto['id'] ?>"><button class="btn btn-sm btn-warning">Cambiar</button></a>
+        <a href="<?php //Eliminar producto con producto['id'] ?>"><button class="btn btn-sm btn-danger">Borrar</button></a>
+      </td>
+    </tr>
+  <?php endforeach ?>
+    </tbody>
+  </table>
+</div>
